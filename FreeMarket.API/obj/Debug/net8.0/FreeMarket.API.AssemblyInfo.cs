acc428c8aae20f169f15779007615eee39b14b25
happymeal <happymeal>
2024-08-26 17:46:50 +0800

    Install packages

--- FreeMarket.API/obj/Debug/net8.0/FreeMarket.API.AssemblyInfo.cs
+++ FreeMarket.API/obj/Debug/net8.0/FreeMarket.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreeMarket.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094b55594c00ffe2e79c9b211db5498bbb595e07")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreeMarket.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FreeMarket.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
