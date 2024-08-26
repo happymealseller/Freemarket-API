d8c766bfea454dc7bf01607296ce66e5494fc879
happymeal <happymeal>
2024-08-26 17:55:10 +0800

    Setup connection to db and di

--- FreeMarket.API/Program.cs
+++ FreeMarket.API/Program.cs
@@ -1,3 +1,6 @@
+using FreeMarket.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<FreeMarketDBContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("WebAPIConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
